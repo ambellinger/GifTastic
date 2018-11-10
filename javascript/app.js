7bfb2b37f5d12b08de9f53fb16897a32f1e44ccd
Lexi <ambellinger24@gmail.com>
2018-11-10 15:00:22 -0600

    need to add styling

--- javascript/app.js
+++ javascript/app.js
@@ -1,9 +1,6 @@
 //Pseudocode -- Homework Week 6 -- GifTastic
-
 //Instructions
-
 //GOAL// The app should take the topics in an array and create buttons in the HTML.
-
 //Create an array with topics saved under var topics
 var topics = ["corgi",
     //"german sherpherd",
@@ -15,9 +12,8 @@ var topics = ["corgi",
 //"border collie",
 //"pit bull",
 //"labrador retriever"]
-
 var dogs;
-
+$(document).ready(function() {
 
 //*//Consult Activity Week 6:7 //*//
 
@@ -41,6 +37,7 @@ function renderButtons() {
         a.text(topics[i]);
         // Adding the button to the HTML
         $("#buttons").append(a);
+        
     }
 
 
@@ -48,11 +45,12 @@ function renderButtons() {
 
     //GOAL// When the user clicks on a button, the page should grab 10 static, 
     //non-animated gif images from the GIPHY API and place them on the page.
-
+   
+    
     //*//Consult Activity Week 6:13 //*//
 
     //Create an onclick function
-    $(".dog").on("click", function () {
+    $(document).on("click", ".dog", function () {
         $("#dog-gifs-appear-here").empty();
         alert("you've clicked");
 
@@ -60,7 +58,7 @@ function renderButtons() {
         // exact topic
 
         var dog = $(this).attr("data-name");
-
+       
         //Create a var queryURL https://.... + topics + apikey… + limit 10. 
         //Topics is the variable. Consult the GIPHY website to see how the url should look.
 
@@ -78,7 +76,7 @@ function renderButtons() {
                 // Storing an array of results in the results variable
                 var results = response.data;
 
-
+                console.log(response.data);
                 //Then, still within the onclick function, create a for loop that will grab the gifs. 
                 for (var i = 0; i < results.length; i++) {
                     console.log(queryURL);
@@ -104,15 +102,20 @@ function renderButtons() {
                     //*This will help later with the animated gif function.
                     dogImage.attr("data-state", "still")
 
+                    dogImage.attr("data-still", results[i].images.original_still.url )
+                    dogImage.attr("data-animate", results[i].images.original.url )
+
+
                     // use .attr to change the src attribute to the still image of the gif. 
                     //In the GIPHY Json there is an a way access the still image going thru the object tree. 
 
-                    dogImage.attr("src", results[i].images.original_still.url);
+                   // dogImage.attr("src", results[i].images.original_still.url);
+                   dogImage.attr("src",  results[i].images.original_still.url);
 
                     //Append both the these to the div
                     gifDiv.append(p);
                     gifDiv.append(dogImage);
-
+                    
 
                     //Write the div onto the html 
                     $("#dog-gifs-appear-here").prepend(gifDiv);
@@ -123,47 +126,56 @@ function renderButtons() {
                 //If the user clicks the gif again, it should stop playing. 
 
                 //create another onclick function that selects everything with the .doggy class.
-                $(".doggy").on("click", function () {
+                $(document).on("click", ".doggy", function () {
 
-                    $.ajax({
-                        url: queryURL,
-                        method: "GET"
-                    })
+                    // var movingGif = results[i].images.original.url;
+                    // var stillGif = results[i].images.original_still.url;
 
-                        .then(function (response) {
-                            var results = response.data;
+
+                       console.log(this);
                             //Within that onclick function, create a variable called state. 
                             //var state = $(this).attr("data-state"); This will add the data state of the 
                             //gif to the variable so that it is easier to switch it. 
-                            var state = $(dogImage).attr("data-state");
-
-                            //Create a for loop so that the index or i is referencing that right bject.
-                            for (var i = 0; i < results.length; i++) {
-                                //save the urls to variable to make it easier to reference it later 
-                                ////reference the place in the object tree for the appropriate gif state's.url    
-                                var movingGif = results[i].images.original.url;
-                                var stillGif = results[i].images.original_still.url;
+                            var state = $(this).attr("data-state");
                                 
                             // If else statement will animate the gif by switching states.
 
                             if (state === "still") {
                                 //this will swtitch the still url to the moving url 
-                                $(dogImage).attr("src", movingGif);
+                               // $(this).attr("src", movingGif);
+                                $(this).attr("src", $(this).attr("data-animate"))
                                 //this will switch the data state from still to animate
-                                $(dogImage).attr("data-state", "animate");
+                                $(this).attr("data-state", "animate");
                             } else {
                                 //this will switch the moving url to the still
-                                $(dogImage).attr("src", stillGif);
+                                $(this).attr("src",  $(this).attr("data-still"));
+
                                 //this will switch the data state from animate to still
-                                $(dogImage).attr("data-state", "still");
+                                $(this).attr("data-state", "still");
                             }
-                        }
-                        })
+                        
+                        
                 });
+                $(".submit").on("click", function(event) {
+                    event.preventDefault();
+                  // This line grabs the input from the textbox
+                    var newDog = $("#topics-input").val();
+               
+                    // Adding movie from the textbox to our array
+                    topics.push(newDog);
+               
+                    // Calling renderButtons which handles the processing of our movie array
+                    renderButtons();
+                     //adding return false prevents the page from reloading with the form button.
+                   return false;
+                  });
             });
     });
 
 }
+
+//Consider activity 6:14
+//Put it all in one function
 
 //*//Only once you get images displaying with button presses should you move on to the next step. //*//
 
@@ -176,17 +188,19 @@ function renderButtons() {
 //Make sure you add a form to your html document.
 
 //Create an onclick function that interacts with the form button
-// $("#topics-input").on("click", function(event) {
-//     event.preventDefault();
-//     // This line grabs the input from the textbox
-//     var newDog = $("#topics-input").val();
+//  $(".submit").on("click", function(event) {
+//      event.preventDefault();
+//    // This line grabs the input from the textbox
+//      var newDog = $("#topics-input").val();
 
-//     // Adding movie from the textbox to our array
-//     topics.push(newDog);
+//      // Adding movie from the textbox to our array
+//      topics.push(newDog);
 
-//     // Calling renderButtons which handles the processing of our movie array
-//     renderButtons();
-//   });
+//      // Calling renderButtons which handles the processing of our movie array
+//      renderButtons();
+//       //adding return false prevents the page from reloading with the form button.
+//     return false;
+//    });
 
 //add this   event.preventDefault(); (?? Not sure why)
 
@@ -194,4 +208,4 @@ function renderButtons() {
 
 //Call the button making function
 renderButtons()
-
+})
